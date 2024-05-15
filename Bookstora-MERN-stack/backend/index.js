9ba8cc5131cd44b90a3ad7e317bdb6d670bc4704
toprak-cavusdag <cavusdagtoprak@gmail.com>
2024-05-15 14:43:33 +0300

    router done

--- Bookstora-MERN-stack/backend/index.js
+++ Bookstora-MERN-stack/backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const mongoose = require("mongoose");
-const Book = require("./models/bookModels.js");
+const booksRoute = require("./routes/booksRouter");
 
 const app = express();
 const port = process.env.PORT;
@@ -11,28 +11,7 @@ app.get("/", (req, res) => {
   return res.status(200).send("Welcome to my backend service");
 });
 
-app.post("/books", async (req, res) => {
-  try {
-    if (!req.body.title || !req.body.author || !req.body.publishYear) {
-      return res.status(500).send({
-        message: "Send all required fields: title, author or publishYear",
-      });
-    }
-
-    const newBook = {
-      title: req.body.title,
-      author: req.body.author,
-      publishYear: req.body.publishYear,
-    };
-
-    const book = await Book.create(newBook);
-
-    return res.status(201).send(book);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send({ message: error.message });
-  }
-});
+app.use("/books", booksRoute);
 
 app.listen(port || 8000, () => {
   console.log(`Server running on the port ${port}...`);
---
+++ Bookstora-MERN-stack/backend/routes/booksRouter.js
@@ -0,0 +1,94 @@
+const express = require("express");
+const Book = require("../models/bookModels.js");
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.author || !req.body.publishYear) {
+      return res.status(500).send({
+        message: "Send all required fields: title, author or publishYear",
+      });
+    }
+
+    const newBook = {
+      title: req.body.title,
+      author: req.body.author,
+      publishYear: req.body.publishYear,
+    };
+
+    const book = await Book.create(newBook);
+
+    return res.status(201).send(book);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: error.message });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.author || !req.body.publishYear) {
+      return res.status(400).send({
+        message: "Send all required fields: title, author or publishYear",
+      });
+    }
+
+    const { id } = req.params;
+
+    const result = await Book.findByIdAndUpdate(id, req.body);
+
+    if (!result) {
+      return res.status(400).json({ message: "Book not found" });
+    }
+    return res.status(200).send({ message: "Book updated successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: error.message });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const result = await Book.findByIdAndDelete(id);
+
+    if (!result) {
+      return res.status(400).json({ message: "Book not found" });
+    }
+    return res.status(200).send({ message: "Book deleted successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: error.message });
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
+    const books = await Book.find({});
+    return res.status(201).send({
+      count: books.length,
+      data: books,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: error.message });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const books = await Book.findById(id);
+    return res.status(201).send({
+      count: books.length,
+      data: books,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: error.message });
+  }
+});
+
+module.exports = router;
