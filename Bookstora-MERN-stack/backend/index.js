5b351bb00d1fec887cc16296e8e583fb1218f836
toprak-cavusdag <cavusdagtoprak@gmail.com>
2024-05-15 11:51:45 +0300

    model created and connected db

--- Bookstora-MERN-stack/backend/index.js
+++ Bookstora-MERN-stack/backend/index.js
@@ -1,12 +1,37 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const mongoose = require("mongoose");
+const Book = require("./models/bookModels.js");
 
 const app = express();
 const port = process.env.PORT;
+app.use(express.json());
 
 app.get("/", (req, res) => {
   return res.status(200).send("Welcome to my backend service");
+});
+
+app.post("/books", async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.author || !req.body.publishYear) {
+      return res.status(500).send({
+        message: "Send all required fields: title, author or publishYear",
+      });
+    }
+
+    const newBook = {
+      title: req.body.title,
+      author: req.body.author,
+      publishYear: req.body.publishYear,
+    };
+
+    const book = await Book.create(newBook);
+
+    return res.status(201).send(book);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: error.message });
+  }
 });
 
 app.listen(port || 8000, () => {
---
+++ Bookstora-MERN-stack/backend/models/bookModels.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const bookSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    publishYear: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Book", bookSchema);
