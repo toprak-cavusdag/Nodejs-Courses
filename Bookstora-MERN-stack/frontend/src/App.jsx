ee34f519574f30071930621c00416e4113810cd4
toprak-cavusdag <cavusdagtoprak@gmail.com>
2024-05-17 08:29:20 +0300

    frontend first

---
+++ Bookstora-MERN-stack/frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+import CreateBook from "./pages/CreateBook";
+import DeleteBook from "./pages/DeleteBook";
+import EditBook from "./pages/EditBook";
+import Home from "./pages/Home";
+import ShowBook from "./pages/ShowBook";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/books/create" element={<CreateBook />} />
+        <Route path="books/details/:id" element={<ShowBook />} />
+        <Route path="/books/edit/:id" element={<EditBook />} />
+        <Route path="/books/delete/:id" element={<DeleteBook />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
